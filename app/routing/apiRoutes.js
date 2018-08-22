f38719cf2caf347d8f2e7f7be3d91772fd5ef863
Stephen Pellegrini <spellegrini156@gmail.com>
2018-08-21 21:39:10 -0700

    added logic to determine closest match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,5 +7,27 @@ module.exports = function(app) {
 
     app.post('/api/friends', function(req, res) {
         friendData.push(req.body);
-    })
+        console.log(req.body);
+        var diffArray = [];
+        for (var i = 0; i < friendData.length; i++) {
+            var totalDifference = 0;
+            if (friendData[i].name !== req.body.name) {
+                for (var j = 0; j < req.body.scores.length; j++) {
+                    totalDifference += Math.abs(parseInt(req.body.scores[j]) - friendData[i].scores[j]);
+                }
+                diffArray.push({index: i, difference: totalDifference});
+            }
+        }
+        console.log(diffArray);
+        var lowestDiff = diffArray[0].difference;
+        var lowestDiffIndex = 0;
+        for (var i = 1; i < diffArray.length; i++) {
+            if (lowestDiff > diffArray[i].difference) {
+                lowestDiff = diffArray[i].difference;
+                lowestDiffIndex = i;
+            }
+        }
+        console.log(lowestDiffIndex, lowestDiff);
+        res.send(friendData[lowestDiffIndex]);
+    });
 };
