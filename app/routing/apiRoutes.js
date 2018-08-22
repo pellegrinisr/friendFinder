1f166221b0b926d6333b8a6db82f7bb6a690b8e4
Stephen Pellegrini <spellegrini156@gmail.com>
2018-08-21 20:10:29 -0700

    wrote event handler for submit button

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,11 @@
 var friendData = require('../data/friends');
 
 module.exports = function(app) {
-    app.get('/api/friendsList', function(req, res) {
+    app.get('/api/friends', function(req, res) {
         res.json(friendData);
     });
 
-    app.post('/api/friendsList', function(req, res) {
+    app.post('/api/friends', function(req, res) {
         friendData.push(req.body);
-        res.json(true);
     })
 };
