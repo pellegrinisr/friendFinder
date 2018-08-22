448396b90178ec3bdce7b8df749ece96d7f75377
Stephen Pellegrini <spellegrini156@gmail.com>
2018-08-21 17:53:11 -0700

    added api routes; added submit button to survey

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,12 @@
+var friendData = require('../data/friends');
+
+module.exports = function(app) {
+    app.get('/api/friendsList', function(req, res) {
+        res.json(friendData);
+    });
+
+    app.post('/api/friendsList', function(req, res) {
+        friendData.push(req.body);
+        res.json(true);
+    })
+};
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
