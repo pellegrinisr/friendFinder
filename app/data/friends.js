e4c3049627fd4fecb9603d9e3a09e604b4f41948
Stephen Pellegrini <spellegrini156@gmail.com>
2018-08-21 19:09:34 -0700

    10 friends added to data set

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+module.exports =
+[
+    {
+        name: 'Elias',
+        photo: "https://media.giphy.com/media/APcFiiTrG0x2/giphy.gif",
+        scores: [ 4, 5, 1, 2, 1, 5, 2, 2, 2, 2 ]
+    },
+    {
+        name: 'Dennis',
+        photo: "https://media.giphy.com/media/1d7F9xyq6j7C1ojbC5/giphy.gif",
+        scores: [ 4, 1, 1, 2, 5, 3, 2, 3, 5, 2 ]
+    },
+    {
+        name: 'Sarah',
+        photo: 'https://media.giphy.com/media/ND6xkVPaj8tHO/giphy.gif',
+        scores: [ 2, 3, 4, 2, 3, 1, 4, 4, 4, 5 ]
+    },
+    {
+        name: 'Amanda',
+        photo: 'https://media.giphy.com/media/G3773sSDJHHy0/giphy.gif',
+        scores: [ 1, 5, 1, 4, 2, 1, 2, 2, 1, 4 ]
+    },
+    {
+        name: 'Margaret',
+        photo: 'https://media.giphy.com/media/6TWkEij704KHu/giphy.gif',
+        scores: [ 4, 2, 1, 5, 2, 4, 3, 2, 5, 1 ]
+    },
+    {
+        name: 'Christopher',
+        photo: 'https://media.giphy.com/media/XKYjR0Hsjh5cs/giphy.gif',
+        scores: [ 4, 2, 2, 3, 2, 4, 5, 4, 5, 3 ]
+    },
+    {
+        name: 'David',
+        photo: 'https://media.giphy.com/media/f1GG38RQLvdgQ/giphy.gif',
+        scores: [ 2, 1, 5, 2, 4, 4, 4, 4, 3, 3 ]
+    },
+    {
+        name: 'Kathy',
+        photo: 'https://media.giphy.com/media/L9E28br6rFvYk/giphy.gif',
+        scores: [ 2, 5, 2, 5, 5, 5, 2, 5, 2, 5 ]
+    },
+    {
+        name: 'Maria',
+        photo: 'https://media.giphy.com/media/26tOZxcbcZzxDlek8/giphy.gif',
+        scores: [ 5, 1, 5, 5, 3, 5, 4, 2, 2, 5 ]
+    },
+    {
+        name: 'Robert',
+        photo: 'https://media.giphy.com/media/TIyJGNK325XGciFEnI/giphy.gif',
+        scores: [ 5, 1, 5, 1, 4, 1, 4, 5, 1, 3 ]
+    }
+];
